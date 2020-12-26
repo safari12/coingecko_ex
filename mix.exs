dc1029cf60a5fbdc0cce9de35be3e009728d7d66
safari <rsafari.s@gmail.com>
2020-12-26 16:12:16 -0600

    elixir 1.11 breaks httpoison tls

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Coingecko.MixProject do
     [
       app: :coingecko,
       version: "1.0.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
